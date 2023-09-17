34e0c3d9a0153ca10828f0cfce57f90758c528b5
david-pawar <135388636+david-pawar@users.noreply.github.com>
2023-09-17 07:27:26 -0400

    all coding done
    event handler is created, output is designed by adding $ sign in the beginning, toFixed has been used and code produces accurate values

--- script.js
+++ script.js
@@ -3,3 +3,20 @@
 	{David Pawar | 0800287 }
 */
 
+$(document).ready(function() {   //it enables to run js only when DOM is fully laoded
+	
+	$('#yearly-salary, #percent').on('change', function() {  //selected elements which have 'yearly-salary' and 'percent' as their id value
+	  // Getting the values from the input fields
+	  let yearlySalary = parseFloat($('#yearly-salary').val()) || 0;
+	  let percent = parseFloat($('#percent').val()) || 0;
+  
+	  // Calculating the tech spending
+	  let techSpending = (yearlySalary * percent) / 100;
+  
+	  // Displaying the tech spending with a dollar sign
+	  $('#amount').text('$' + techSpending.toFixed(2));
+	});
+  });
+  
+
+//  event 'change' runs code whens triggered when the value of an input element is changed and the element loses focus while event 'keyup' runs code  each time a key on the keyboard is released after being pressed
